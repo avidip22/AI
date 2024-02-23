62f4c124b6bf4c1f40d13044e9325e595d7d131e
AVISEK MANDAL <136554833+avidip22@users.noreply.github.com>
2024-02-23 14:25:46 +0530

    ARTIFICIAL INTELLIGENCEassignment-1.py

--- ARTIFICIAL INTELLIGENCE.py
+++ ARTIFICIAL INTELLIGENCE.py
@@ -1,4 +1,4 @@
-# QUESTION
+# QUESTION-1
 # A class with 10 students wants to produce some information from the results of the four standard
 # tests in Maths, Science, English and IT. Each test is out of 100 marks. The information output
 # should be the highest, lowest and average mark for each test and the highest, lowest and average
